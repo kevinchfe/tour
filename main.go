b8c3b29c628802ab3a7cf7715bd59b55f694f15f
kevinchfe <546598819@qq.com>
2020-10-11 15:06:30 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	var name string
+
+	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+	goCmd.StringVar(&name, "name", "go语言", "帮助信息")
+	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+	phpCmd.StringVar(&name, "name", "php语言", "帮助信息")
+
+	args := flag.Args()
+	switch args[0] {
+	case "go":
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		_ = phpCmd.Parse(args[1:])
+	}
+
+	log.Printf("name: %s", name)
+}
